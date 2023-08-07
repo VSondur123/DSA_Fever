f0a8be79ad4bef98dfa1e5a9894eda3f5bead21e
VSondur123 <varunsondur@gmail.com>
2023-08-07 19:32:56 +0530

    changed to find the subarray

--- array/easy/maximum_subarray.py
+++ array/easy/maximum_subarray.py
@@ -1,18 +1,25 @@
+nums = [-2,1,-3,4,-1,2,1,-5,4]
 mx=0
 sm=0
 cnt=0
-nums = [-2,-6,-1]
-for i in nums:
+l,h=0,0
+x=[]
+for i in range(len(nums)):
     #Condition for case if all numbers are negative
-    if i<0:
+    if nums[i]<0:
         cnt+=1
     #Cumulative sum till sum < 0, then initialise sum = 0
-    sm += i
+    sm += nums[i]
     if sm < 0:
         sm = 0
-
+        l = i+1
+        x.append(0)
+    if sm > mx:
+        h=i
     mx = max(sm,mx)
-
+    x.append(sm)
+print(x)
+print(nums[l:h+1]) #sub array 
 if cnt == len(nums):
     print(max(nums))
 else:
